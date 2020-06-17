18ec8f223db00bde4580cd659e0004c4e48ef9a0
Higor Tolentino Andrade <higorandrade100@gmail.com>
2020-06-17 15:03:43 +0000

    Header.Js

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 
 function Header() {
-
     return (
         <AppBar position="fixed">
             <Toolbar>
